4d7d034749025d1c064f276a400d2fe0bea0d440
Karun-lab <karunashok16@gmail.com>
2024-12-02 09:54:35 +0100

    removed and remaned files

--- FK_Alex.m
+++ FK_Alex.m
@@ -1,46 +1,28 @@
 
+%show(alex)
+%interactiveGUI = interactiveRigidBodyTree(alex)
+
 clear;
-alex= importrobot('Alex-Main-URDF.urdf');
-% Define link offsets
-d1=124.41;    d2=-15.64;  d3=17.75;
-% Define link length
-a1=0;  a2=0;   a3=176.95;
-% Define link twist
-al1=-90;    al2=180;  al3=0;
+clc;
 
-%Input the angles for the motors
-J=input("Enter Joint angles: ");
-q1=J(1,1);
-q2=J(1,2);
-q3=J(1,3);
-
-
-%Transformation for each joint angles
-T0_1 = [cosd(q1),  -sind(q1)*cosd(al1),      sind(q1)*sind(al1),     a1*(cosd(q1))
-        sind(q1),   cosd(q1)*cosd(al1),     -cosd(q1)*sind(al1),     a1*(sind(q1))
-         0,                  sind(al1),               cosd(al1),     d1
-         0,                          0,                       0,     1];
-
-T1_2 = [cosd(q2),  -sind(q2)*cosd(al2),      sind(q2)*sind(al2),     a2*(cosd(q2))
-        sind(q2),   cosd(q2)*cosd(al2),     -cosd(q2)*sind(al2),     a2*(sind(q2))
-         0,                  sind(al2),               cosd(al2),     d2
-         0,                          0,                       0,     1];
-
-T2_3 = [cosd(q3),  -sind(q3)*cosd(al3),      sind(q3)*sind(al3),     a3*(cosd(q3))
-        sind(q3),   cosd(q3)*cosd(al3),     -cosd(q3)*sind(al3),     a3*(sind(q3))
-         0,                  sind(al3),               cosd(al3),     d3
-         0,                          0,                       0,     1];
-
-%Multiply all transformations to get Homogenous matrix
-H= (T0_1*T1_2*T2_3);
-
-disp("Honogenous matrix= ");
-disp(H);
-
+alex = importrobot('Alex-Main-URDF.urdf');
 config = homeConfiguration(alex);
-config(1).JointPosition = q1*0.0174533;
-config(2).JointPosition = q2*0.0174533;
-config(3).JointPosition = q3*0.0174533;
 
-showdetails(alex)
-show(alex,config);
+figure;
+robotPlot = show(alex, config);
+showdetails(alex);
+
+
+    j1 = input('Joint angle 1: ');
+    j2 = input('Joint angle 2: ');
+    j3 = input('Joint angle 3: ');
+    
+    config(1).JointPosition = j1 * 0.0174533;
+    config(2).JointPosition = j2 * 0.001;
+    config(3).JointPosition = j3 * 0.0174533;
+    
+    disp(robotPlot);
+    
+    axis([-0.5 0.5 -0.5 0.5 0 0.5]);
+
+hold on
